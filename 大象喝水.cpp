6f50f168150a7172fb22d6ad63f93d7e5ce5225c
bamiaoyu <3556864467@qq.com>
2024-10-24 15:03:20 +0800

    水代码

---
+++ 大象喝水.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int h,r,n=1;
+    cin>>h>>r;
+    double s=3.14*h*r*r/1000;
+    int d=(20.0/s+0.5);
+    cout<<d;
+    return 0;
+}
